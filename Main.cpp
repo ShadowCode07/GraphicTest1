f55b17882d1d44a9bcf00674d0a8076f5c990a00
ShadowCode007 <82028748+ShadowCode07@users.noreply.github.com>
2022-01-19 20:13:30 +0200

    Add comments

--- Main.cpp
+++ Main.cpp
@@ -4,34 +4,47 @@
 
 int main()
 {
+	// Initialize GLFW
 	glfwInit();
 
+	// Tell GLFW what version of OpenGL we are using
+	// In this cse its OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// That means we have only modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Ctrate a GLFWwindow object
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Window", NULL, NULL);
+	// Checks if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	// Introoduce window into current contex
 	glfwMakeContextCurrent(window);
 
+	//Load GLAD to configure OpenGL
 	gladLoadGL();
 
 	glViewport(0, 0, 800, 800);
 
 	glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	//Swap back and front buffer
 	glfwSwapBuffers(window);
 
+	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
+	// Terminate the window
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
